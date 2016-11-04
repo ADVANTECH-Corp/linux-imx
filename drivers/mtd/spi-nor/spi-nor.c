329de37a739b2815f469f99d0f580d2183629d4e
alex6235 <alex.cheng@advantech.com.tw>
2016-11-04 15:27:59 +0800

    0015_SPI_001_add_spi_id

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -529,6 +529,11 @@ static int spi_nor_unlock(struct mtd_info *mtd, loff_t ofs, uint64_t len)
  * have been converging on command sets which including JEDEC ID.
  */
 static const struct spi_device_id spi_nor_ids[] = {
+#ifdef CONFIG_ARCH_ADVANTECH
+	/* Micron N25Q */
+	{ "n25q", INFO(0x20ba16, 0, 64 * 1024,  64, SECT_4K) },
+	{ "n25q", INFO(0x20bb16, 0, 64 * 1024,  64, SECT_4K) },
+#endif
 	/* Atmel -- some are (confusingly) marketed as "DataFlash" */
 	{ "at25fs010",  INFO(0x1f6601, 0, 32 * 1024,   4, SECT_4K) },
 	{ "at25fs040",  INFO(0x1f6604, 0, 64 * 1024,   8, SECT_4K) },
