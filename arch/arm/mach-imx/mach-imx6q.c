bc1ac3e2d01eb05fbaa33074e806b9def70fe31f
alex6235 <alex.cheng@advantech.com.tw>
2016-11-04 15:47:52 +0800

    0030_Ethernet_002_Fixed_LAN_issue_for_SI_test

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -113,9 +113,11 @@ static int ar8031_phy_fixup(struct phy_device *dev)
 {
 	u16 val;
 
+#ifndef CONFIG_ARCH_ADVANTECH
 	/* Set RGMII IO voltage to 1.8V */
 	phy_write(dev, 0x1d, 0x1f);
 	phy_write(dev, 0x1e, 0x8);
+#endif
 
 	/* disable phy AR8031 SmartEEE function. */
 	phy_write(dev, 0xd, 0x3);
