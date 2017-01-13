ccb51057688d32b587e8191d0bc2f0ac0b7e2d78
alex6235 <alex.cheng@advantech.com.tw>
2017-01-13 19:58:56 +0800

    fixed reboot failed issue

--- drivers/watchdog/watchdog_advantech.c
+++ drivers/watchdog/watchdog_advantech.c
@@ -493,7 +493,7 @@ static void __exit adv_wdt_i2c_exit(void)
 	i2c_del_driver(&adv_wdt_i2c_driver);
 }
 
-module_init(adv_wdt_i2c_init);
+rootfs_initcall(adv_wdt_i2c_init);
 module_exit(adv_wdt_i2c_exit);
 
 MODULE_DESCRIPTION("Advantech Watchdog I2C Driver");
