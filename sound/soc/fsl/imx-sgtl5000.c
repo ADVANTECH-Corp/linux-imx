0bdb6165b45960d5a6a11410eedade5d04795cae
HBW <winston.on.job@everyone.tw>
2019-08-01 14:48:37 +0800

    [iMX8] To use CONFIG_ARCH_ADVANTECH only; dts renamed

--- sound/soc/fsl/imx-sgtl5000.c
+++ sound/soc/fsl/imx-sgtl5000.c
@@ -65,13 +65,15 @@ static int imx_sgtl5000_probe(struct platform_device *pdev)
 	int int_port, ext_port;
 	int ret;
 
-#ifdef CONFIG_ARCH_ADVANTECH_IMX8QM
-	if (of_find_property(np, "no-audmux", NULL)) {
-	   dev_info(&pdev->dev, "no-audmux specified, skipping audmux configuration\n");
-	   goto audmux_config_done;
+	if (of_machine_is_compatible("fsl,imx8qm")) {
+		if (of_find_property(np, "no-audmux", NULL)) {
+		dev_info(&pdev->dev, "no-audmux specified, skipping audmux configuration\n");
+		goto audmux_config_done;
+		}
 	}
-#endif
-#ifndef CONFIG_ARCH_ADVANTECH_IMX8MQ
+	if (of_machine_is_compatible("fsl,imx8mq")) {
+		goto audmux_config_done;
+	}
 	ret = of_property_read_u32(np, "mux-int-port", &int_port);
 	if (ret) {
 		dev_err(&pdev->dev, "mux-int-port missing or invalid\n");
@@ -107,10 +109,9 @@ static int imx_sgtl5000_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "audmux external port setup failed\n");
 		return ret;
 	}
-#endif
-#ifdef CONFIG_ARCH_ADVANTECH_IMX8QM
+
 audmux_config_done:
-#endif
+
 	ssi_np = of_parse_phandle(pdev->dev.of_node, "ssi-controller", 0);
 	codec_np = of_parse_phandle(pdev->dev.of_node, "audio-codec", 0);
 	if (!ssi_np || !codec_np) {
