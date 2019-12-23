9dd780b65cf087549e7528481ce68f5bc686c806
timliangtw <tim.liang@advantech.com.tw>
2019-12-23 17:32:04 +0800

    mipi to hdmi: modify pd-gpios setting

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1063,7 +1063,22 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 		adv7511->type = id->driver_data;
 
 	memset(&link_config, 0, sizeof(link_config));
+#ifdef	CONFIG_ARCH_ADVANTECH	//enable vdd adv7511_init_regulators 
+	/*
+	 * The power down GPIO is optional. If present, toggle it from active to
+	 * inactive to wake up the encoder.
+	 */
+	adv7511->gpio_pd = devm_gpiod_get_optional(dev, "pd", GPIOD_OUT_HIGH);
+	if (IS_ERR(adv7511->gpio_pd)) {
+		ret = PTR_ERR(adv7511->gpio_pd);
+		goto uninit_regulators;
+	}
 
+	if (adv7511->gpio_pd) {
+		mdelay(5);
+		gpiod_set_value_cansleep(adv7511->gpio_pd, 0);
+	}
+#endif
 	if (adv7511->type == ADV7511)
 		ret = adv7511_parse_dt(dev->of_node, &link_config);
 	else
@@ -1091,7 +1106,7 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 		pkt_i2c_addr = adv7511->addr_pkt << 1;
 	else
 		adv7511->addr_pkt = pkt_i2c_addr >> 1;
-
+#ifndef	CONFIG_ARCH_ADVANTECH
 	/*
 	 * The power down GPIO is optional. If present, toggle it from active to
 	 * inactive to wake up the encoder.
@@ -1106,7 +1121,7 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 		mdelay(5);
 		gpiod_set_value_cansleep(adv7511->gpio_pd, 0);
 	}
-
+#endif
 	adv7511->regmap = devm_regmap_init_i2c(i2c, &adv7511_regmap_config);
 	if (IS_ERR(adv7511->regmap)) {
 		ret = PTR_ERR(adv7511->regmap);
