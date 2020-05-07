ADVANTECH-Corp/linux-imx6

0010_GPIO_002_Read_GPIO_DR_instead_of_GPIO_PSR

drivers/gpio/gpio-mxc.c
@@ -447,10 +447,18 @@ static int mxc_gpio_probe(struct platform_device *pdev)
 		}
 	}
 
+#ifdef CONFIG_ARCH_ADVANTECH
+	err = bgpio_init(&port->bgc, &pdev->dev, 4,
+			 port->base + GPIO_DR,
+			 port->base + GPIO_DR, NULL,
+			 port->base + GPIO_GDIR, NULL, 0);
+#else
 	err = bgpio_init(&port->bgc, &pdev->dev, 4,
 			 port->base + GPIO_PSR,
 			 port->base + GPIO_DR, NULL,
 			 port->base + GPIO_GDIR, NULL, 0);
+#endif
+
 	if (err)
 		goto out_bgio;
 

ADVANTECH-Corp/linux-imx

drivers/gpio/gpio-mxc.c
@@ -655,11 +655,20 @@ static int mxc_gpio_probe(struct platform_device *pdev)
 							 port);
 	}
 
+#ifdef CONFIG_ARCH_ADVANTECH
+	err = bgpio_init(&port->gc, &pdev->dev, 4,
+                         port->base + GPIO_DR,
+                         port->base + GPIO_DR, NULL,
+                         port->base + GPIO_GDIR, NULL,
+                         BGPIOF_READ_OUTPUT_REG_SET);
+#else
 	err = bgpio_init(&port->gc, &pdev->dev, 4,
 			 port->base + GPIO_PSR,
 			 port->base + GPIO_DR, NULL,
 			 port->base + GPIO_GDIR, NULL,
 			 BGPIOF_READ_OUTPUT_REG_SET);
+#endif
+
 	if (err)
 		goto out_bgio;
 