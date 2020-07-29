cc1277a277026a170daee263560d0cf834d64a3f
Darren Huang <Darren.Huang@advantech.com.tw>
2020-07-29 15:49:42 +0800

    [RSB-3720] Enable the PCIE Mini Card

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -113,6 +113,7 @@ struct imx6_pcie {
 	int			reset_gpio;
 #ifdef CONFIG_ARCH_ADVANTECH
 	int			power_on_gpio;
+	int			usb_host_pwr_en_gpio;
 	int                     reset_time;
 #endif
 	bool			gpio_active_high;
@@ -2534,6 +2535,20 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 		}
 	} else if (imx6_pcie->power_on_gpio == -EPROBE_DEFER) {
 		return imx6_pcie->power_on_gpio;
+	}
+
+	imx6_pcie->usb_host_pwr_en_gpio = of_get_named_gpio(node, "usb-host-pwr-en", 0);
+	if (gpio_is_valid(imx6_pcie->usb_host_pwr_en_gpio)) {
+		ret = devm_gpio_request_one(&pdev->dev,
+					    imx6_pcie->usb_host_pwr_en_gpio,
+					    GPIOF_OUT_INIT_HIGH,
+					    "usb-host-pwr-en");
+		if (ret) {
+			dev_err(&pdev->dev, "unable to get usb-host-pwr-en gpio\n");
+			return ret;
+		}
+	} else if (imx6_pcie->usb_host_pwr_en_gpio == -EPROBE_DEFER) {
+		return imx6_pcie->usb_host_pwr_en_gpio;
 	}
 #endif
 
