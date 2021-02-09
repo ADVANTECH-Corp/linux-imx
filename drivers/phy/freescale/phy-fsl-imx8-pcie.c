931300c0ab51c3f49d43ecbd0c7c2d38699a087a
Darren Huang <darren.huang@advantech.com.tw>
2021-02-09 15:19:59 +0800

    [RSB-3720] patch from 5.4.70 to fix PCIe Signal Integrity gen 3 Tx EQ preset test fail

--- drivers/phy/freescale/phy-fsl-imx8-pcie.c
+++ drivers/phy/freescale/phy-fsl-imx8-pcie.c
@@ -11,12 +11,103 @@
 #include <linux/platform_device.h>
 
 #define PHY_PLL_LOCK_WAIT_MAX_RETRIES	2000
+#define IMX8MP_PCIE_PHY_FLAG_EXT_OSC	BIT(0)
+
+#define IMX8MP_PCIE_PHY_CMN_REG020	0x80
+#define  PLL_ANA_LPF_R_SEL_FINE_0_4	0x04
+#define IMX8MP_PCIE_PHY_CMN_REG061	0x184
+#define  ANA_PLL_CLK_OUT_TO_EXT_IO_EN	BIT(0)
+#define IMX8MP_PCIE_PHY_CMN_REG062	0x188
+#define  ANA_PLL_CLK_OUT_TO_EXT_IO_SEL	BIT(3)
+#define IMX8MP_PCIE_PHY_CMN_REG063	0x18C
+#define  AUX_PLL_REFCLK_SEL_SYS_PLL	GENMASK(7, 6)
+#define IMX8MP_PCIE_PHY_CMN_REG064	0x190
+#define  ANA_AUX_RX_TX_SEL_TX		BIT(7)
+#define  ANA_AUX_RX_TERM_GND_EN		BIT(3)
+#define  ANA_AUX_TX_TERM		BIT(2)
+#define IMX8MP_PCIE_PHY_CMN_REG065	0x194
+#define  ANA_AUX_RX_TERM		(BIT(7) | BIT(4))
+#define  ANA_AUX_TX_LVL			GENMASK(3, 0)
+#define IMX8MP_PCIE_PHY_CMN_REG076	0x200
+#define  LANE_RESET_MUX_SEL		0x00
+#define IMX8MP_PCIE_PHY_CMN_REG078	0x208
+#define  LANE_TX_DATA_CLK_MUX_SEL	0x00
+
+#define IMX8MP_PCIE_PHY_TRSV_REG001	0x404
+#define  LN0_OVRD_TX_DRV_LVL		0x3F
+#define IMX8MP_PCIE_PHY_TRSV_REG005	0x414
+#define  LN0_OVRD_TX_DRV_PST_LVL_G1	0x2B
+#define IMX8MP_PCIE_PHY_TRSV_REG006	0x418
+#define  LN0_OVRD_TX_DRV_PST_LVL_G2	0x3
+#define IMX8MP_PCIE_PHY_TRSV_REG007	0x41C
+#define  LN0_OVRD_TX_DRV_PST_LVL_G3	0xA
+#define IMX8MP_PCIE_PHY_TRSV_REG009	0x424
+#define  LN0_OVRD_TX_DRV_PRE_LVL_G1	0x10
+#define IMX8MP_PCIE_PHY_TRSV_REG059	0x4EC
+#define  LN0_OVRD_RX_CTLE_RS1_G1	0x13
+#define IMX8MP_PCIE_PHY_TRSV_REG060	0x4F0
+#define  LN0_OVRD_RX_CTLE_RS1_G2_G3	0x25
+#define IMX8MP_PCIE_PHY_TRSV_REG069	0x514
+#define  LN0_ANA_RX_CTLE_IBLEED		0x7
+#define IMX8MP_PCIE_PHY_TRSV_REG107	0x5AC
+#define  LN0_OVRD_RX_RTERM_VCM_EN	0xB8
+#define IMX8MP_PCIE_PHY_TRSV_REG109	0x5B4
+#define  LN0_ANA_OVRD_RX_SQHS_DIFN_OC	0xD4
+#define IMX8MP_PCIE_PHY_TRSV_REG110	0x5B8
+#define  LN0_ANA_OVRD_RX_SQHS_DIFP_OC	0x6A
+#define IMX8MP_PCIE_PHY_TRSV_REG158	0x678
+#define  LN0_RX_CDR_FBB_FINE_G1_G2	0x55
+#define IMX8MP_PCIE_PHY_TRSV_REG159	0x67C
+#define  LN0_RX_CDR_FBB_FINE_G3_G4	0x53
+#define IMX8MP_PCIE_PHY_TRSV_REG206	0x738
+#define  LN0_TG_RX_SIGVAL_LBF_DELAY	0x4
+
+#ifdef CONFIG_ARCH_ADVANTECH
+int tx_drv_lvl_ctrl_g1 = 0;	//For SI test
+#endif
 
 struct imx8_pcie_phy {
 	struct phy *phy;
 	struct clk *clk;
 	void __iomem *base;
+	u32 flags;
 };
+
+#ifdef CONFIG_ARCH_ADVANTECH
+static void imx8_pcie_phy_cal(struct phy *phy)
+{
+	u32 value;
+	struct imx8_pcie_phy *imx8_phy = phy_get_drvdata(phy);
+	/* export clock to ep when internal clock is used as PHY REF clock */
+	if ((imx8_phy->flags & IMX8MP_PCIE_PHY_FLAG_EXT_OSC) == 0) {
+		writel(ANA_PLL_CLK_OUT_TO_EXT_IO_EN,
+		       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG061);
+		writel(ANA_PLL_CLK_OUT_TO_EXT_IO_SEL,
+		       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG062);
+		writel(AUX_PLL_REFCLK_SEL_SYS_PLL,
+		       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG063);
+		value = ANA_AUX_RX_TX_SEL_TX | ANA_AUX_TX_TERM;
+		writel(value | ANA_AUX_RX_TERM_GND_EN,
+		       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG064);
+		writel(ANA_AUX_RX_TERM | ANA_AUX_TX_LVL,
+		       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG065);
+	}
+
+	/*
+	 * Fine tune the parameters of the PHY, let PCIe link up to GEN3
+	 * between two EVK boards in the EP/RC validation system.
+	 */
+	writel(tx_drv_lvl_ctrl_g1,
+	       imx8_phy->base + IMX8MP_PCIE_PHY_TRSV_REG001);
+
+	writel(PLL_ANA_LPF_R_SEL_FINE_0_4,
+	       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG020);
+	writel(LANE_RESET_MUX_SEL,
+	       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG076);
+	writel(LANE_TX_DATA_CLK_MUX_SEL,
+	       imx8_phy->base + IMX8MP_PCIE_PHY_CMN_REG078);
+}
+#endif
 
 static int imx8_pcie_phy_init(struct phy *phy)
 {
@@ -42,6 +133,7 @@ static int imx8_pcie_phy_init(struct phy *phy)
 	 * operation for GEN3 cannot be achieved with the SW workaround
 	 * since the buffer structure cannot be bypassed in GEN3 mode.
 	 */
+
 	/* wait for pipe0_clk locked by checking status from PCS. */
 	for (retries = 0; retries < PHY_PLL_LOCK_WAIT_MAX_RETRIES;
 	     retries++) {
@@ -51,15 +143,14 @@ static int imx8_pcie_phy_init(struct phy *phy)
 		udelay(10);
 	}
 
-	/* setup_deskew_fifo_bypass to workaround ERR050442 */
-	writel(0x32, imx8_phy->base + 0xd8);
-	writel(0x1, imx8_phy->base + 0x480);
-	writel(0x0, imx8_phy->base + 0x488);
-	writel(0x4, imx8_phy->base + 0x73c);
-	writel(0x4, imx8_phy->base + 0x6ec);
+	if (retries >= PHY_PLL_LOCK_WAIT_MAX_RETRIES) {
+		pr_info("pcie phy pipe clk is not ready\n");
+		return -ETIMEDOUT;
+	}
 
-	/* Configure TX drive level  */
-	writel(0x2d, imx8_phy->base + 0x404);
+#ifdef CONFIG_ARCH_ADVANTECH
+	imx8_pcie_phy_cal(phy);
+#endif
 
 	return 0;
 }
@@ -89,14 +180,36 @@ static struct phy_ops imx8_pcie_phy_ops = {
 
 static int imx8_pcie_phy_probe(struct platform_device *pdev)
 {
+	u32 val = 0;
 	struct phy_provider *phy_provider;
 	struct device *dev = &pdev->dev;
+	struct device_node *np = dev->of_node;
 	struct imx8_pcie_phy *imx8_phy;
 	struct resource *res;
-
+#ifdef CONFIG_ARCH_ADVANTECH
+	int ret;
+#endif
 	imx8_phy = devm_kzalloc(dev, sizeof(*imx8_phy), GFP_KERNEL);
 	if (!imx8_phy)
 		return -ENOMEM;
+
+	imx8_phy->flags &= ~IMX8MP_PCIE_PHY_FLAG_EXT_OSC;
+	if (of_property_read_u32(np, "ext_osc", &val) < 0)
+		/*
+		 * Not specify ext_osc, use the external OSC as default
+		 * CLK mode.
+		 */
+		imx8_phy->flags |= IMX8MP_PCIE_PHY_FLAG_EXT_OSC;
+	if (val == 0)
+		imx8_phy->flags &= ~IMX8MP_PCIE_PHY_FLAG_EXT_OSC;
+	else if (val == 1)
+		imx8_phy->flags |= IMX8MP_PCIE_PHY_FLAG_EXT_OSC;
+	else
+		dev_info(dev, "invalid clk mode %d.\n", val);
+
+#ifdef CONFIG_ARCH_ADVANTECH
+	ret = of_property_read_u32(np, "tx_drv_lvl_ctrl_g1", &tx_drv_lvl_ctrl_g1);
+#endif
 
 	imx8_phy->clk = devm_clk_get(dev, "phy");
 	if (IS_ERR(imx8_phy->clk)) {
