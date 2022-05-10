a96d93704304cd3fac478626260a9286348785bd
Darren Huang <darren.huang@advantech.com.tw>
2022-05-10 17:16:35 +0800

    [ROM-5721] enable the dsi to edp bridge IC sn65dsi86.
    	1. Support the non-ASSR LCD pannel.
    	2. Support the standard 1080p timming.

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -3377,6 +3377,47 @@ static const struct panel_desc arm_rtsm = {
 	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
 };
 
+#ifdef	CONFIG_ARCH_ADVANTECH
+static const struct display_timing std_1080p_timing = {
+        .pixelclock = { 148500000, 148500000, 148500000 },
+        .hactive = { 1920, 1920, 1920 },
+        .hfront_porch = { 88, 88, 88 },
+        .hsync_len = { 44, 44, 44 },
+        .hback_porch = { 148, 148, 148 },
+        .vactive = { 1080, 1080, 1080 },
+        .vfront_porch = { 4, 4, 4 },
+        .vsync_len = { 5, 5, 5 },
+        .vback_porch = { 36, 36, 36 },
+        .flags = DISPLAY_FLAGS_HSYNC_LOW |
+                 DISPLAY_FLAGS_VSYNC_LOW |
+                 DISPLAY_FLAGS_DE_LOW |
+                 DISPLAY_FLAGS_PIXDATA_NEGEDGE,
+};
+
+static const struct panel_desc std_1080p = {
+        .timings = &std_1080p_timing,
+        .num_timings = 1,
+        .bpc = 8,
+        .size = {
+                .width = 160,
+                .height = 90,
+        },
+        .delay = {
+                /*
+                 * The panel spec recommends one second delay
+                 * to the below items.  However, it's a bit too
+                 * long in pratical.  Based on tests, it turns
+                 * out 100 milliseconds is fine.
+                 */
+                .prepare = 100,
+                .enable = 100,
+                .unprepare = 100,
+                .disable = 100,
+        },
+        .bus_format = MEDIA_BUS_FMT_RGB888_1X24,//MEDIA_BUS_FMT_RGB888_1X7X4_SPWG,
+};
+#endif
+
 static const struct of_device_id platform_of_match[] = {
 	{
 		.compatible = "ampire,am-480272h3tmqw-t01h",
@@ -3713,7 +3754,14 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "winstar,wf35ltiacd",
 		.data = &winstar_wf35ltiacd,
-	}, {
+	},
+#ifdef	CONFIG_ARCH_ADVANTECH
+	{
+		.compatible = "std,1080p",
+		.data = &std_1080p,
+	},
+#endif
+	{
 		/* sentinel */
 	}
 };
