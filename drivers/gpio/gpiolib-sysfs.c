e0f6e2af1dd5cc4e5d27aa7da3853dc5f25de5a4
yanwei.cao <yanwei.cao@advantech.com.cn>
2022-06-10 14:37:32 +0800

    add gpio_attrs:num to check gpio original num

--- drivers/gpio/gpiolib-sysfs.c
+++ drivers/gpio/gpiolib-sysfs.c
@@ -362,6 +362,25 @@ static ssize_t active_low_store(struct device *dev,
 }
 static DEVICE_ATTR_RW(active_low);
 
+#ifdef CONFIG_ARCH_ADVANTECH
+static ssize_t num_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct gpiod_data *data = dev_get_drvdata(dev);
+	struct gpio_desc *desc = data->desc;
+	ssize_t			status;
+
+	mutex_lock(&data->mutex);
+
+	status = sprintf(buf, "%d\n",desc_to_gpio(desc));
+
+	mutex_unlock(&data->mutex);
+
+	return status;
+}
+static DEVICE_ATTR_RO(num);
+#endif
+
 static umode_t gpio_is_visible(struct kobject *kobj, struct attribute *attr,
 			       int n)
 {
@@ -389,6 +408,9 @@ static struct attribute *gpio_attrs[] = {
 	&dev_attr_edge.attr,
 	&dev_attr_value.attr,
 	&dev_attr_active_low.attr,
+#ifdef CONFIG_ARCH_ADVANTECH
+	&dev_attr_num.attr,
+#endif
 	NULL,
 };
 
