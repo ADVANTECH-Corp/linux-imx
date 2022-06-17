d3c43015bd2b5f5cf86eb8a1b3e4670a7509f28e
Darren Huang <darren.huang@advantech.com.tw>
2022-06-17 16:50:58 +0800

    [ROM-5722] Fix the otg mode fail

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -84,11 +84,13 @@ static int dwc3_get_dr_mode(struct dwc3 *dwc)
 		 * mode. If the controller supports DRD but the dr_mode is not
 		 * specified or set to OTG, then set the mode to peripheral.
 		 */
+#ifndef CONFIG_ARCH_ADVANTECH
 		if (mode == USB_DR_MODE_OTG &&
 		    (!IS_ENABLED(CONFIG_USB_ROLE_SWITCH) ||
 		     !device_property_read_bool(dwc->dev, "usb-role-switch")) &&
 		    !DWC3_VER_IS_PRIOR(DWC3, 330A))
 			mode = USB_DR_MODE_PERIPHERAL;
+#endif
 	}
 
 	if (mode != dwc->dr_mode) {
