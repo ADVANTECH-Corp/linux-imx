e970874b0f34b33a683760b2202ddc77410f12de
yanwei.cao <yanwei.cao@advantech.com.cn>
2022-07-06 17:17:38 +0800

    [rsb3730] disable mii clock to meet PHY spec

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3897,7 +3897,8 @@ fec_probe(struct platform_device *pdev)
 
 	//disable MII clock to meet PHY poweron sequence
 #ifdef CONFIG_ARCH_ADVANTECH
-	writel(fep->phy_speed, fep->hwp + FEC_MII_SPEED);
+	if (of_get_property(np, "adv,disable-mii-clk", NULL))
+		writel(fep->phy_speed, fep->hwp + FEC_MII_SPEED);
 #endif
 
 	ret = fec_reset_phy(pdev);
