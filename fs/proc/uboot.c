1d5cde87494c3e7dcfc9bb834af13c045612c895
clayderhua <clayderhua@gmail.com>
2023-05-26 17:56:33 +0800

    [All]fix boardinfo build errors

--- fs/proc/uboot.c
+++ fs/proc/uboot.c
@@ -27,11 +27,11 @@ static int uboot_version_proc_open(struct inode *inode, struct file *file)
 	return single_open(file, uboot_version_proc_show, NULL);
 }
 
-static const struct file_operations uboot_version_proc_fops = {
-	.open		= uboot_version_proc_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
+static const struct proc_ops uboot_version_proc_fops = {
+	.proc_open		= uboot_version_proc_open,
+	.proc_read		= seq_read,
+	.proc_lseek		= seq_lseek,
+	.proc_release	= single_release,
 };
 
 static int __init proc_uboot_version_init(void)
