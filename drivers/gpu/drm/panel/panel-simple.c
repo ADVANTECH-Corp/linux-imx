8b2ba1ed657007b5943940e7610bc929d899b0bc
SeanChuang2 <dinner0828@gmail.com>
2025-02-17 17:22:20 +0800

    [LVDS] driver: Add innolux,g101icel01 panel to panel-simple.c

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1335,6 +1335,36 @@ static const struct panel_desc idk_2112p_k2xga1 = {
 	.bus_flags = DRM_BUS_FLAG_DE_HIGH,
 	.connector_type = DRM_MODE_CONNECTOR_LVDS,
 };
+
+static const struct drm_display_mode innolux_g101ice_l01_new_mode = {
+	.clock = 71100,
+	.hdisplay = 1280,
+	.hsync_start = 1280 + 80,
+	.hsync_end = 1280 + 80 + 1,
+	.htotal = 1280 + 80 + 1 + 79,
+	.vdisplay = 800,
+	.vsync_start = 800 + 11,
+	.vsync_end = 800 + 11 + 1,
+	.vtotal = 800 + 11 + 1 + 11,
+	.flags = DRM_MODE_FLAG_NHSYNC | DRM_MODE_FLAG_NVSYNC,
+};
+
+static const struct panel_desc innolux_g101ice_l01_new = {
+	.modes = &innolux_g101ice_l01_new_mode,
+	.num_modes = 1,
+	.bpc = 8,
+	.size = {
+		.width = 217,
+		.height = 135,
+	},
+	.delay = {
+		.enable = 200,
+		.disable = 200,
+	},
+	.bus_format = MEDIA_BUS_FMT_RGB888_1X7X4_SPWG,
+	.bus_flags = DRM_BUS_FLAG_DE_HIGH,
+	.connector_type = DRM_MODE_CONNECTOR_LVDS,
+};
 #endif
 
 static const struct drm_display_mode boe_hv070wsa_mode = {
@@ -4191,6 +4221,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "idk,2112p-k2xga1",
 		.data = &idk_2112p_k2xga1,
+	}, {
+		.compatible = "innolux,g101ice-l01_new",
+		.data = &innolux_g101ice_l01_new
 	}, {
 #endif
 		.compatible = "boe,hv070wsa-100",
