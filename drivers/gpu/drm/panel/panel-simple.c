563c43ca30a861df656298df04f5b3094e046522
Hank.Hsueh <Hank.Hsueh@advantech.com.tw>
2025-11-13 16:06:36 +0800

    [drm][panel-simple][AUO G070VW01] Switch LVDS mapping to SPWG (RGB888_1X7X4_SPWG)
    Change auo_g070vw01_v0 bus_format from JEIDA to SPWG to match panel wiring and correct color channel order.
    
    Effect: fixes inverted/incorrect colors on LVDS for G070VW01_V0.
    
    Files: drivers/gpu/drm/panel/panel-simple.c

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1361,7 +1361,7 @@ static const struct panel_desc auo_g070vw01_v0 = {
                 .width = 152,
                 .height = 91,
         },
-        .bus_format = MEDIA_BUS_FMT_RGB888_1X7X4_JEIDA,
+        .bus_format = MEDIA_BUS_FMT_RGB888_1X7X4_SPWG,
         .bus_flags = DRM_BUS_FLAG_DE_HIGH,
         .connector_type = DRM_MODE_CONNECTOR_LVDS,
 };
